d6cc59b1228636887dfd58a49a6a88669d36b439
Ashwath Sreedhar Halemane <ashwathsh@Ashwaths-MacBook-Pro.local>
2023-06-14 15:29:26 -0700

    yet to work on tokens to be gathered from env var or secure location not hardcoded

--- create-jira-tickets-from-snyk.py
+++ create-jira-tickets-from-snyk.py
@@ -4,11 +4,11 @@ import os
 
 # get the Snyk API token from the environment
 snyk_token = os.environ.get("SNYK_API_TOKEN")
-org_id= os.environ.get("synk_org_id_infinitus") # Infinitus org
-project_id= os.environ.get("snyk_absinthe_code_analysis_project_id") # Absinthe code analysis 
-
-
-snyk_api_url="https://snyk.io/api/v1/org/"+str(org_id)+"/project/"+str(project_id)
+# org_id= os.environ.get("synk_org_id_infinitus") # Infinitus org
+org_id = "0ab5aa02-0901-47b0-bf3e-f515c14981c5"
+# project_id= os.environ.get("snyk_absinthe_code_analysis_project_id") # Absinthe code analysis 
+project_id = "5dccaa30-d108-4db1-81e1-e361561a48fe"
+snyk_api_url="https://snyk.io/api/v1/org/"+org_id+"/project/"+project_id
 
 headers = {
     "Authorization": "token " + str(snyk_token)
